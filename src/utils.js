5ff3aa2a777b1387400d6e09f9619e387174cdc0
Kenji <k.saito.1985@gmail.com>
2017-12-22 17:50:30 +0100

    fix generateWireframeIndices

--- src/utils.js
+++ src/utils.js
@@ -6,9 +6,15 @@
 export function generateWireframeIndices(indices) {
 	let wireframeIndices = [];
 
-	for (let ii = 0; ii < indices.length - 1; ii++) {
-		wireframeIndices.push(indices[ii]);
-		wireframeIndices.push(indices[ii + 1]);
+	for (let ii = 0; ii < indices.length / 3; ii++) {
+		wireframeIndices.push(indices[3 * ii]);
+		wireframeIndices.push(indices[3 * ii + 1]);
+
+		wireframeIndices.push(indices[3 * ii + 1]);
+		wireframeIndices.push(indices[3 * ii + 2]);
+
+		wireframeIndices.push(indices[3 * ii + 2]);
+		wireframeIndices.push(indices[3 * ii]);
 	}
 
 	wireframeIndices = new Uint16Array(wireframeIndices);
